f58e20da2a0a97bbd2dc4c4e1e6bd7c65029029e
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-07-08 08:05:56 -0500

    Update the endpoints' port and order

--- curl-test.sh
+++ curl-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl --request GET http://localhost:5001/api/timeline_post
-curl -X POST http://localhost:5001/api/timeline_post -d 'name=Alana&email=alana@example.com&content=Hej! Hur mår du?'
-curl -X DELETE http://localhost:5001/api/timeline_post
+curl -X POST http://localhost:5000/api/timeline_post -d 'name=Alana&email=alana@example.com&content=Hej! Hur mår du?'
+curl --request GET http://localhost:5000/api/timeline_post
+curl -X DELETE http://localhost:5000/api/timeline_post
