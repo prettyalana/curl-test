01a2ca5852cba32ea82d6b84736b56049fd85eef
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-07-08 10:32:34 -0500

    Update port number for assignment - port 5000 is used by Airplay and causes an error

--- curl-test.sh
+++ curl-test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-curl -X POST http://localhost:5001/api/timeline_post -d 'name=Alana&email=alana@example.com&content=Hej! Hur mår du?'
-curl --request GET http://localhost:5001/api/timeline_post | jq 'keys'
+curl -X POST http://localhost:5000/api/timeline_post -d 'name=Alana&email=alana@example.com&content=Hej! Hur mår du?'
+curl --request GET http://localhost:5000/api/timeline_post | jq 'keys'
 
-for id in $(curl --request GET http://localhost:5001/api/timeline_post | jq -r '.timeline_posts[].id'); 
+for id in $(curl --request GET http://localhost:5000/api/timeline_post | jq -r '.timeline_posts[].id'); 
 do
-    curl -X DELETE http://localhost:5001/api/timeline_post/$id
+    curl -X DELETE http://localhost:5000/api/timeline_post/$id
 done
